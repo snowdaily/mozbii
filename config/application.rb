87ce4ec71555da91c16598932f133e02de026802
snowdaily <snow.daily@gmail.com>
2014-06-23 22:29:16 +0800

    add assets settings

--- config/application.rb
+++ config/application.rb
@@ -20,13 +20,16 @@ module Mozbii
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
     # Explicitly register the extensions we are interested in compiling
     config.assets.precompile.push(Proc.new do |path|
-    File.extname(path).in? [
-      '.html', '.erb', '.haml',                 # Templates
-      '.png',  '.gif', '.jpg', '.jpeg',         # Images
-      '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-    ]
+        File.extname(path).in? [
+          '.html', '.erb', '.haml',                 # Templates
+          '.png',  '.gif', '.jpg', '.jpeg',         # Images
+          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
     end)
   end
 end
