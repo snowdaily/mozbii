c7b83fb434483b357a83248f8afd658d55fe0156
snowdaily <snow.daily@gmail.com>
2014-06-23 21:40:52 +0800

    commit

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,7 +15,7 @@ role :db,  %w{mozbii@staging.mozbii.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'mozbii.com', user: 'mozbii', roles: %w{web app}, my_property: :my_value
+server 'staging.mozbii.com', user: 'mozbii', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
