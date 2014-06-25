3134ed87022e15d2851668d399e47148728d8b08
snowdaily <snow.daily@gmail.com>
2014-06-25 18:57:55 +0800

    fix deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,10 +27,10 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml, config/environments/development.rb, config/environments/production.rb}
+set :linked_files, %w{config/database.yml config/environments/development.rb config/environments/production.rb}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
